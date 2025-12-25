31a493959e068b5e7f033fbdf3f532d27eac095c
Samyukta Athreya <athreya.samyukta@gmail.com>
2025-12-25 21:13:30 +0000

    connected frontend to backend for post pins

--- backend/repos/memory_repo.py
+++ backend/repos/memory_repo.py
@@ -1,18 +1,18 @@
 from schemas import PinCreate, PinOut, PinsResponse
 
-memory_db = {}
+pins = []
 
 def get_pins_from_db():
     return PinsResponse(
-            pins=memory_db,
-            total=len(memory_db)
+            listOfPins=pins,
+            total=len(pins)
         )
 
 def add_pin_to_db(newPin):
-    memory_db.append(newPin)
+    pins.append(newPin)
 
 def get_pin_from_db(pin_id: str):
-    for pin in memory_db:
+    for pin in pins:
         if pin.id == pin_id:
             return pin
     return None
